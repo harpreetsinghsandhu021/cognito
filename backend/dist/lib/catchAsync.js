44898e7d94951e580cf99273b7cec4841258c479
Harpreet Singh <110158082+harpreetsinghsandhu021@users.noreply.github.com>
2025-06-11 09:06:35 +0530

    Delete backend/dist directory

--- backend/dist/lib/catchAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
-function default_1(fn) {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
-}
