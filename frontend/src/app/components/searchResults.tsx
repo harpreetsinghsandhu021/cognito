c0ad54693540cbbd1d769a8683c3d2646a77d862
Harpreet Singh <harpreetsinghsandhu021@gmail.com>
2025-06-17 23:09:37 +0530

    Refactor search results to use client-side data fetching
    Moves search functionality from server component to client component for better UX:
    - Adds loading state and spinner during data fetching
    - Implements real-time search results updates
    - Wraps page in Suspense for smooth transitions
    
    This change improves the search experience by providing immediate feedback and eliminating full page reloads when performing searches.

--- frontend/src/app/components/searchResults.tsx
+++ frontend/src/app/components/searchResults.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BentoGrid, BentoGridItem } from "./ui/bentogrid";
 import { Paper, PublishedDate } from "../interfaces";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+import Loader from "./ui/loader";
 
-const SearchResults = ({ items }: { items: Paper[] }) => {
+const SearchResults = () => {
+  const searchParams = useSearchParams();
+  const [items, setItems] = useState<Paper[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const query = searchParams.get("query");
+
+  async function fetchResults(query: string) {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/papers/search/semantic?limit=10&size=20&query=${query}`
+      );
+
+      if (res.status == 200) {
+        setItems(res.data.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    if (query) {
+      fetchResults(query);
+    }
+  }, [query]);
+
   return (
     <>
+      {loading && <Loader className="my-20" />}
+
       <BentoGrid className="max-w-5xl max-lg:max-w-3xl mt-20 mx-auto gap-6 md:auto-rows-[15rem]">
         {items &&
           items.length !== 0 &&
+          !loading &&
           items.map((item: Paper, i) => (
             <BentoGridItem
               key={i}
